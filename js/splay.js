4a3f10ed3e8ca39a2ec6358dddb617c8b8ee87ca
Shawn Moore <shawn.moore10@gmail.com>
2016-04-15 17:41:39 -0700

    splaying operations passed initial testing, need to install splay inside BST operations

--- js/splay.js
+++ js/splay.js
@@ -1,17 +1,14 @@
 /*
-Splay Tree
+JavaScript Splay Tree
 
 Description:
-A Splay Tree is a tree that moves recently access nodes to the root
-using a process of tree rotations.  The process overall is known as "splaying".
+  A Splay Tree is a tree that rotates recently accessed nodes to the root.  
+  The root rotation process is known as "splaying".
 
 Purpose:
-Popular nodes will be quicker and more efficiently accessed.  If a tree contains
-thousands of nodes however only 10% of those are regularly used, then it is
-more efficient to keep them closer to the root.
-
-author: 
-  Shawn Moore
+  Popular nodes will be quicker and more efficiently accessed.  
+  If a tree contains many nodes however only a small percentage are regularly used, 
+  then it is more efficient to keep the recently accessed nodes closer to the root.
 
 Methods:
   search
@@ -20,6 +17,8 @@ Methods:
   min
   max
   inOrder
+  rotateRight
+  rotateLeft
   splay
 
 Node properties:
@@ -28,28 +27,16 @@ Node properties:
   left
   right
 
-Splaying:
-  To “splay node x”, traverse up the tree from
-  node x to root, rotating along the way until x
-  is the root.
+Author:
+  Shawn Moore
 
-  x === chosen node
+Original Binary Search Tree structure modeled after Java implementation by:
+  Robert Sedgewick
+  algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/BST.java
 
-  For each rotation:
-    1. If x is the root, do nothing.
-       
-    2. "Zig": If x has no grandparent, rotate x about its parent.
-    3. If x has a grandparent:
-      a. "Zig-Zig": if x and its parent are both left children or both right
-         children, rotate the parent about the grandparent, then
-         rotate x about its parent.
-      b. "Zig-Zag": if x and its parent are opposite type children (one left
-         and the other right), rotate x about its parent, then rotate
-         x about its new parent (former grandparent).
-
-Reference:
-http://cs.brynmawr.edu/Courses/cs206/fall2012/slides/09_SplayTrees.pdf
-
+Splay Tree Operations modeled after Java implementation by:
+  Josh Israel
+  algs4.cs.princeton.edu/33balanced/SplayBST.java
 */
 
 //Modify null checks to instead use a more general sense of "undefined"
@@ -64,7 +51,7 @@ function Node(key,val) {
 }
 
 //Binary Search Tree Constructor
-function Bst() {
+function splayBst() {
   this.root = null;
 }
 
@@ -73,8 +60,7 @@ function Bst() {
 //retrieves a node
 //traverses tree recursively from root
 //based on key
-Bst.prototype.search = function(k) {
-  var current;
+splayBst.prototype.search = function(k) {
   var searchRecursive = function(cNode, key) {
     if (cNode === null) 
       return null;
@@ -90,8 +76,7 @@ Bst.prototype.search = function(k) {
   if (this.root === null || k === null)
     return null;
 
-  current = this.root;
-  return searchRecursive(current,k);
+  return searchRecursive(this.root,k);
 };
 
 //Put Node on Tree
@@ -99,8 +84,7 @@ Bst.prototype.search = function(k) {
 //adds node to the correct location on tree;
 //traverses tree recursively from root
 //based on key
-Bst.prototype.insert = function(k,v) {
-  var current;
+splayBst.prototype.insert = function(k,v) {
   var insertRecursive = function(cNode, key, val) {
     //return a new node when current node is null
     //this is what generates the inserted node
@@ -148,8 +132,7 @@ Bst.prototype.insert = function(k,v) {
   }
 
   //begin at root
-  current = this.root;
-  return insertRecursive(current,k,v);
+  return insertRecursive(this.root,k,v);
 };
 
 // * Removal Only Reassigns left and right links for the tree *
@@ -163,8 +146,7 @@ Bst.prototype.insert = function(k,v) {
 //    - find min child from it's right subtree (min(node.right); i.e. successor)
 //    - replace removal node's data (key,val) with min child data (they are now duplicates)
 //    - remove the min child node (assign it to null)
-Bst.prototype.remove = function(k) {
-  var current;
+splayBst.prototype.remove = function(k) {
   var removeRecursive = function(cNode, key) {
     var temp;
     if (cNode === null) 
@@ -220,7 +202,7 @@ Bst.prototype.remove = function(k) {
 //Get the Minimum Node
 //
 //key based
-Bst.prototype.min = function(n) {
+splayBst.prototype.min = function(n) {
   var current;
   var minRecursive = function(cNode) {
     if (cNode.left) {
@@ -243,7 +225,7 @@ Bst.prototype.min = function(n) {
 //Get the Maximum Node
 //
 //key based
-Bst.prototype.max = function(n) {
+splayBst.prototype.max = function(n) {
   var current;
   var maxRecursive = function(cNode) {
     if (cNode.right) {
@@ -271,7 +253,7 @@ Bst.prototype.max = function(n) {
 //then perform action on the parent node
 //then traverse to the first right child node
 //and begin the process again
-Bst.prototype.inOrder = function(cNode,nodeArray) {
+splayBst.prototype.inOrder = function(cNode,nodeArray) {
   if (!Array.isArray(nodeArray)) {
     throw new Error("inOrder requires array");
   }
@@ -285,7 +267,7 @@ Bst.prototype.inOrder = function(cNode,nodeArray) {
 
 //Rotate node to the right
 //(becomes right child of its left child)
-Bst.prototype.rotateRight = function(cNode) {
+splayBst.prototype.rotateRight = function(cNode) {
   var temp;
   if (cNode) {
     temp = cNode.left;
@@ -297,7 +279,7 @@ Bst.prototype.rotateRight = function(cNode) {
 
 //Rotate node to the left
 //(becomes left child of its right child)
-Bst.prototype.rotateLeft = function(cNode) {
+splayBst.prototype.rotateLeft = function(cNode) {
   var temp;
   if (cNode) {
     temp = cNode.right;
@@ -307,79 +289,115 @@ Bst.prototype.rotateLeft = function(cNode) {
   return temp;
 }
 
+//Splaying:
+//  To “splay node x”, traverse up the tree from
+//  node x to root, rotating along the way until x
+//  is the root.
+//
+//  x === chosen node
+//
+//  For each rotation:
+//    1. If x is the root, do nothing.
+//    2. "Zig": If x has no grandparent, rotate x about its parent.
+//    3. If x has a grandparent:
+//      a. "Zig-Zig": if x and its parent are both left children or both right
+//         children, rotate the parent about the grandparent, then
+//         rotate x about its parent.
+//      b. "Zig-Zag": if x and its parent are opposite type children (one left
+//         and the other right), rotate x about its parent, then rotate
+//         x about its new parent (former grandparent).
+//
+// Reference:
+// http://cs.brynmawr.edu/Courses/cs206/fall2012/slides/09_SplayTrees.pdf
+//
+// Code below is a modification of original Java splaying code by:
+//   Josh Israel
+//   http://algs4.cs.princeton.edu/33balanced/SplayBST.java
+splayBst.prototype.splay = function(k) {
+  var splayRecursive = function(cNode, key) {
+    //if empty return
+    if (cNode === null)
+      return null;
 
-//CONVERT THE C SPLAY BELOW INTO A JS SPLAY
+    //go left
+    if (key < cNode.key) {
+      //Key is not in tree, exit
+      if (cNode.left === null) 
+        return cNode;
 
-// This function brings the key at root if key is present in tree.
-// If key is not present, then it brings the last accessed item at
-// root.  This function modifies the tree and returns the new root
-struct node *splay(struct node *root, int key)
-{
-    // Base cases: root is NULL or key is present at root
-    if (root == NULL || root->key == key)
-        return root;
- 
-    // Key lies in left subtree
-    if (root->key > key)
-    {
-        // Key is not in tree, we are done
-        if (root->left == NULL) return root;
- 
-        // Zig-Zig (Left Left)
-        if (root->left->key > key)
-        {
-            // First recursively bring the key as root of left-left
-            root->left->left = splay(root->left->left, key);
- 
-            // Do first rotation for root, second rotation is done after else
-            root = rightRotate(root);
-        }
-        else if (root->left->key < key) // Zig-Zag (Left Right)
-        {
-            // First recursively bring the key as root of left-right
-            root->left->right = splay(root->left->right, key);
- 
-            // Do first rotation for root->left
-            if (root->left->right != NULL)
-                root->left = leftRotate(root->left);
-        }
- 
-        // Do second rotation for root
-        return (root->left == NULL)? root: rightRotate(root);
+      //Zig-Zig (i.e. left left)
+      if (key < cNode.left.key) {
+        //Recursively get node matching key
+        cNode.left.left = splayRecursive(cNode.left.left, key);
+        //Rotate parent around grandparent
+        cNode = this.rotateRight(cNode);
+
+      //Zig-Zag (i.e. left right)
+      } else if (key > cNode.left.key) {
+        //Recursively get node matching key
+        cNode.left.right = splayRecursive(cNode.left.right, key);
+        //Rotate x around current parent
+        if (cNode.left.right !== null)
+          cNode.left = this.rotateLeft(cNode.left);
+      }
+
+      //Rotate x around current parent
+      if (cNode.left === null)
+        return cNode;
+      else 
+        return this.rotateRight(cNode);
+
+    //go right
+    } else if (key > cNode.key) {
+      // Key is not in tree, exit
+      if (cNode.right === null) 
+        return cNode;
+
+      //Zig-Zig (i.e. right right)
+      if (key > cNode.right.key) {
+        //Recursively get node matching key
+        cNode.right.right = splayRecursive(cNode.right.right, key);
+        //Rotate parent around grandparent
+        cNode = this.rotateLeft(cNode);
+
+      //Zig-Zag (i.e. right left)
+      } else if (key < cNode.right.key) {
+        //Recursively get node matching key
+        cNode.right.left = splayRecursive(cNode.right.left, key);
+        //Rotate x around current parent
+        if (cNode.right.left !== null)
+          cNode.right = this.rotateRight(cNode.right);
+      }
+
+      //Rotate x around current parent
+      if (cNode.right === null)
+        return cNode;
+      else 
+        return this.rotateLeft(cNode);
+    
+    //key must be equal
+    } else {
+      return cNode;
     }
-    else // Key lies in right subtree
-    {
-        // Key is not in tree, we are done
-        if (root->right == NULL) return root;
- 
-        // Zag-Zig (Right Left)
-        if (root->right->key > key)
-        {
-            // Bring the key as root of right-left
-            root->right->left = splay(root->right->left, key);
- 
-            // Do first rotation for root->right
-            if (root->right->left != NULL)
-                root->right = rightRotate(root->right);
-        }
-        else if (root->right->key < key)// Zag-Zag (Right Right)
-        {
-            // Bring the key as root of right-right and do first rotation
-            root->right->right = splay(root->right->right, key);
-            root = leftRotate(root);
-        }
- 
-        // Do second rotation for root
-        return (root->right == NULL)? root: leftRotate(root);
-    }
+
+  }.bind(this);
+
+  if (this.root === null || k === null)
+    return null;
+  
+  //final result of recursive calls
+  //tree "splayed" around new root
+  this.root = splayRecursive(this.root,k);
+  return this.root;
 }
+
 
 
 
 window.onload = function() {
   var nodes = [];
   //setup
-  var tree = new Bst();
+  var tree = new splayBst();
   tree.insert(10,"shawn");
   tree.insert(4,"addison");
   tree.insert(12,"craig");
@@ -393,15 +411,11 @@ window.onload = function() {
   tree.insert(20,"alexa");
 
 
-  console.log("remove 19");
-  tree.remove(19);
+  console.log("splay 19");
+  tree.splay(19);
   tree.inOrder(tree.root,nodes);
-  console.log("after");
   console.log(nodes);
-
-  console.log(tree.search(19));
-  console.log(tree.search(50));
-
+  console.log(tree.root);
 
 };
 
